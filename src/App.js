bb98cc5886cfca8f62b2b43b43f3daf764d4f8fc
Jithmi Chathurangika <162092982+Jithmi01@users.noreply.github.com>
2025-01-07 12:51:42 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar  from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+      
+
+  );
+}
+
+export default App;
